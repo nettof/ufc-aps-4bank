c44beae59050e7bebce4b85929fce7e092722f7a
nettof <31311970+nettof@users.noreply.github.com>
2017-12-13 11:53:43 -0200

    Add files via upload

---
+++ code/Codigo-fonte CDU/FourBank/src/br/com/fourbank/util/Saque.java
@@ -0,0 +1,37 @@
+package br.com.fourbank.util;
+
+public class Saque extends Movimento implements Transacao{
+	private Conta conta;
+	private float valor;
+	
+	public Saque(Conta conta, float valor) {
+		this.conta = conta;
+		this.valor = valor;
+	}
+	
+	public Saque(String descricao, float valor) {
+		this.setDescricao(descricao);
+		this.setValor(valor);
+	}
+
+	public Conta getConta() {
+		return conta;
+	}
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+
+	public float getValorSaque() {
+		return valor;
+	}
+
+	public void setValorSaque(float valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public void transacao() {
+		conta.setSaldo(conta.getSaldo() - valor);
+	}
+}
