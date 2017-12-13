d52795a2dfec7080683fecc538c775789a149164
nettof <31311970+nettof@users.noreply.github.com>
2017-12-13 12:04:48 -0200

    Delete TransacaoAdapter.java

--- code/GOF/adapter/TransacaoAdapter.java
+++
@@ -1,7 +0,0 @@
-package br.com.fourbank.adapter;
-
-import br.com.fourbank.util.Conta;
-
-public interface TransacaoAdapter {
-     public void transferencia(Conta orig, Conta destino, float valor);
-}
